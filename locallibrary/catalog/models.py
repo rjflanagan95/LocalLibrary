da2209bdcb624a611f3c464ae237438e199a7e3c
Robert Flanagan <robertflanagan95@gmail.com>
2019-07-02 20:41:15 -0400

    language model challenge

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -25,6 +25,7 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
         # """String for representing the Model object."""
@@ -80,3 +81,12 @@ class Author(models.Model):
     def __str__(self):
         # """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
