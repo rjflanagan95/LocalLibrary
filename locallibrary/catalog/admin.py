379bd4b8190de73f09cb89bc78139d8885bc09a3
Robert Flanagan <robertflanagan95@gmail.com>
2019-07-04 22:59:41 -0400

    registered language with admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from catalog.models import Author, Genre, Book, BookInstance
+from catalog.models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+# Register with model
+# the following two lines do the same thing
+# @admin.register(Author, AuthorAdmin)
+admin.site.register(Author, AuthorAdmin)
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(Language)
+# admin.site.register(BookInstance)
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
