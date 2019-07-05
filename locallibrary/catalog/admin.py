c0e51353c23948844649c0562818b4b076068dd6
Robert Flanagan <robertflanagan95@gmail.com>
2019-07-04 23:18:29 -0400

    completed tutorial challenges for the admin page

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-
-# Register your models here.
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
-# admin.site.register(Book)
-# admin.site.register(Author)
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
 
 # the following two lines do the same thing
 # @admin.register(Author, AuthorAdmin)
@@ -31,6 +32,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
