14291eda6515ed13aa7e3383e6acdc7530a32996
Robert Flanagan <robertflanagan95@gmail.com>
2019-08-10 17:30:26 -0400

    author CRUD views with permission mixin

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -122,15 +122,18 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
-class AuthorCreate(CreateView):
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.can_mark_returned'
     model = Author
     fields = '__all__'
     initial = {'date_of_death': '05/01/2018'}
 
-class AuthorUpdate(UpdateView):
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.can_mark_returned'
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
 
-class AuthorDelete(DeleteView):
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.can_mark_returned'
     model = Author
     success_url = reverse_lazy('authors')
