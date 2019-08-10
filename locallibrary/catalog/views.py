da654b3a5b712881be668d0be665da1bda8055e3
Robert Flanagan <robertflanagan95@gmail.com>
2019-08-10 17:16:46 -0400

    author create, update, delete templates

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -8,6 +8,10 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
 def index(request):
     """View function for home page of site."""
 
@@ -117,3 +121,16 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
